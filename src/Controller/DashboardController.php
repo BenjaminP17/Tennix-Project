4553b33307fa98ab6e1f6474d0b862e82918d238
Benjamin Pelissier <benjpls@gmail.com>
2024-03-05 12:32:03 +0100

    ajout template liste de  matchs v1

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class DashboardController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/dashboard', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'DashboardController',
         ]);
     }
 }
