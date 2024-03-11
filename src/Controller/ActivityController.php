b0b66279f6e55b2e1e2605be1d54952f28624c45
Benjamin Pelissier <benjpls@gmail.com>
2024-03-11 10:41:12 +0100

    affichage données matchs

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Rencontre;
+use App\Repository\RencontreRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ActivityController extends AbstractController
 {
-    #[Route('/matchs', name: 'app_matchs')]
-    public function index(): Response
+    #[Route('/matchs', name: 'app_matchs', methods: ['GET'])]
+    public function index(
+        RencontreRepository $RencontreRepository
+    ): Response
     {
+
+        $rencontres = $RencontreRepository->findAll();
+
         return $this->render('activity/matchs.html.twig', [
-            'controller_name' => 'ActivityController',
+            'rencontres'=> $rencontres,
         ]);
     }
 }
