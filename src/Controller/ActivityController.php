7404597bdb7c5fdd029cc89e93186649bc92664f
Benjamin Pelissier <benjpls@gmail.com>
2024-03-11 17:07:05 +0100

    pagination sur page palmares

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -4,19 +4,29 @@ namespace App\Controller;
 
 use App\Entity\Rencontre;
 use App\Repository\RencontreRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ActivityController extends AbstractController
 {
     #[Route('/matchs', name: 'app_matchs', methods: ['GET'])]
     public function index(
-        RencontreRepository $RencontreRepository
+        RencontreRepository $RencontreRepository,
+        PaginatorInterface $paginator,
+        Request $request
     ): Response
     {
 
-        $rencontres = $RencontreRepository->findAll();
+        $data = $RencontreRepository->findAll();
+        $rencontres = $paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            2
+        );
+        
 
         return $this->render('activity/matchs.html.twig', [
             'rencontres'=> $rencontres,
